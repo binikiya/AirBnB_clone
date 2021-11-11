0eb33f0799008bdcfb030282cdae368d5bfe7daf
Elijahdanie <elijahdanie@gmail.com>
2021-11-11 11:41:28 +0100

    finnished number 6

--- console.py
+++ console.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
 
 """
-
+This module implements the HBnB console
 """
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
-        self.prompt = "(hbnb)"
-        
+        self.prompt = "(hbnb) "
 
-    def help(self):
+    def emptyline(self) -> bool:
         pass
 
-    def quitcmd(self):
-        pass
+    def do_quit(self, args):
+        """Quit command to exit program
+        """
+        return True
+
+    def do_EOF(self):
+        """Executes EOF signal to quit program
+        """
+        print("")
+        return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
