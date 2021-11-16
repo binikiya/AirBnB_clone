b8fb5cf7d939d8dab886b89054facf117b345122
binikiya <binikiya123@gmail.com>
2021-11-16 00:31:34 -0800

    Test BaseModel Update

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,35 +2,39 @@
 """
 unittest that tests the console for the AirBnB website
 """
-from models.base_model import BaseModel
-from models.user import User
-from models.city import City
-from models.state import State
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+import inspect
+import console
+import pep8
 import unittest
+HBNBCommand = console.HBNBCommand
 
 
-class TestConsole(unittest.TestCase):
-    """checks if all classes created correctly"""
+class TestConsoleDocs(unittest.TestCase):
+    """Class for testing docs of Console"""
 
-    def TestClasses(self):
-        self.assertEqual(City().__class__.__name__, "City")
-        self.assertEqual(User().__class__.__name__, "User")
-        self.assertEqual(State().__class__.__name__, "State")
-        self.assertEqual(Amenity().__class__.__name__, "Amenity")
-        self.assertEqual(Place().__class__.__name__, "Place")
-        self.assertEqual(Review().__class__.__name__, "Review")
+    def test_pep8_conformance_console(self):
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def TestSubclassed(self):
-        """Checks if the classes are subclassed correctly"""
-        self.assertTrue(issubclass(User().__class__, BaseModel))
-        self.assertTrue(issubclass(City().__class__, BaseModel))
-        self.assertTrue(issubclass(State().__class__, BaseModel))
-        self.assertTrue(issubclass(Amenity().__class__, BaseModel))
-        self.assertTrue(issubclass(Place().__class__, BaseModel))
-        self.assertTrue(issubclass(Review().__class__, BaseModel))
+    def test_pep8_conformance_test_console(self):
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_console_module_docstring(self):
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
+
+    def test_HBNBCommand_class_docstring(self):
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
 
 if __name__ == "__main__":
     unittest.main()
